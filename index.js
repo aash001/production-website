f992f50f1083159491de9ec3f0c6651a21a5af03
aampinar <98187277+aampinar@users.noreply.github.com>
2022-04-23 15:17:11 -0400

    Update HTML, CSS, and JS files. Add movie pics and hero image

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ fetch(url)
         const movieImageArray = parsedReponse.map(movie => movie.image)
         movieImageArray.forEach(movieUrl => {
             const img = document.createElement("img")
+            const movieDesc = document.createElement("p")
+            img.classList.add('movieThumb')
             img.src = `${movieUrl}`;
             movieListing.append(img)
             console.log(parsedReponse)
